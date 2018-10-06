12d1f251a4e9aacfe12ccef7a971d9544ac16de9
Lucas Allison <lucasgallison@gmail.com>
2018-10-06 23:58:18 +0200

    This is a working version of numbersFile

--- numbersFile.cpp
+++ numbersFile.cpp
@@ -9,19 +9,23 @@ ofstream uitvoer;
 char kar;
 int getalFile = 0;
  
-invoer.open("/home/pi/cpp/invoer.txt");
+invoer.open("..\\invoer.txt");
 kar = invoer.get();
 
 while (! invoer.eof()) {
-	if (kar == '0' || kar == '1' || kar == '2' || kar == '3' || kar == '4' || kar == '5' || kar == '6' || kar == '7' || kar == '8' || kar == '9') {
-	cout << kar << endl;	
-	getalFile = getalFile * 10 + kar;
-	cout << getalFile << endl;
-	kar = invoer.get();
+	if (isdigit(kar)) {
+        int getal = (int)kar - 48;
+        getalFile = getalFile * 10 + getal;
+        kar = invoer.get();
 	} else  {
-	cout << getalFile * getalFile << endl;	
-	kar = invoer.get();
-	}
+	    if (getalFile == 0) {
+            kar = invoer.get();
+	    } else {
+            cout << "Dit is het kwadraad van uw getal (" << getalFile << "): " << getalFile * getalFile << endl;
+            getalFile = 0;
+            kar = invoer.get();
+        }//else
+	}//else
 }//while
 
 invoer.close();
