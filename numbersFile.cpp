a38e94640b2cea79b733a816edadd6548ac52559
Lucas Allison <lucasgallison@gmail.com>
2018-10-08 17:00:53 +0200

    added coding to file

--- numbersFile.cpp
+++ numbersFile.cpp
@@ -8,33 +8,62 @@ ifstream invoer;
 ofstream uitvoer;
 char kar;
 int getalFile = 0;
+int pincode = 3285;
+int digit1;
+int digit2;
+int digit3;
+int digit4;
+int codePlek = 0;
  
-invoer.open("/home/pi/cppProjects/invoer.txt");
-uitvoer.open("/home/pi/cppProjects/uitvoer.txt");
+invoer.open("..\\invoer.txt");
+uitvoer.open("..\\uitvoer.txt");
 kar = invoer.get();
 
 if (invoer.fail()) {
 cout  << "De file kan niet geopend worden." << endl;
 return 1 ;
-}//if
+}
+
+
+    digit1 = pincode / 1000;
+    digit2 = (pincode / 100) % 10;
+    digit3 = (pincode / 10) % 10;
+    digit4 = pincode % 10;
+
 
 while (! invoer.eof()) {
-uitvoer.put(kar);
 
-	if (isdigit(kar)) {
+    if (isdigit(kar)) {
         int getal = (int)kar - 48;
         getalFile = getalFile * 10 + getal;
-        kar = invoer.get();
-	} else  {
-	    if (getalFile == 0) {
-            kar = invoer.get();
-	    } else {
+    } else  {
+        if ( ! getalFile == 0) {
             cout << "Dit is het kwadraad van uw getal (" << getalFile << "): " << getalFile * getalFile << endl;
             getalFile = 0;
-            kar = invoer.get();
-        }//else
-	}//else
-}//while
+        }
+    }
+
+    if (kar != '\n') {
+        if (codePlek == 0) {
+            kar = (kar + digit1) % 128;
+        }
+        if (codePlek == 1) {
+            kar = (kar + digit2) % 128;
+        }
+        if (codePlek == 2) {
+            kar = (kar + digit3) % 128;
+        }
+        if (codePlek == 3) {
+            kar = (kar + digit4) % 128;
+        }
+    }
+
+    codePlek++;
+    codePlek = codePlek % 4;
+
+	uitvoer.put(kar);
+	kar = invoer.get();
+}
 
 invoer.close();
 uitvoer.close();
