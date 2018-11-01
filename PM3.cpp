fcf259cf263393d7136e51c0a6fcbb36c73945bb
Lucas Allison <Lucasgallison@gmail.com>
2018-11-01 18:09:50 +0100

    First steps

--- PM3.cpp
+++ PM3.cpp
@@ -3,54 +3,60 @@ using namespace std;
 
 
 
-int hoofdmenu () {
+void submenu () {
 
-    char keuzeGebruiker;
+    char keuzeSubmenu;
 
-    cout << "Maak een keuze uit: s(C)hoon, (R)andom, (P)arameters of (S)toppen" << endl;
-    cin >> keuzeGebruiker;
+    cout << "Welkom in het submenu. Maak een keuze uit: A, B of T" << endl;
 
     do {
-            switch (keuzeGebruiker) {
+        cin >> keuzeSubmenu;
+        switch (keuzeSubmenu) {
 
-                case 'C':
-                    cout << "U heeft C/c geantwoord" << endl;
+            case 'A': case 'a':
+                cout << "U heeft A/a geantwoord" << endl;
                 break;
-                case 'c':
-                    cout << "U heeft C/c geantwoord" << endl;
+            case 'B': case 'b':
+                cout << "U heeft B/b geantwoord" << endl;
                 break;
-                case 'R':
-                    cout << "U heeft R/r geantwoord" << endl;
+            case 'T':  case 't':
                 break;
-                case 'r':
-                    cout << "U heeft R/r geantwoord" << endl;
-                break;
-                case 'P':
-                    cout << "U heeft P/p geantwoord" << endl;
-                break;
-                case 'p':
-                    cout << "U heeft P/p geantwoord" << endl;
-                break;
-                case 'S':
-                    return 1;
-                case 's':
-                    return 1;
-                default:
-                    cout << "Vul een valide letter in;";
-                    cin >> keuzeGebruiker;
-            }//switch
+            default:
+                cout << "Vul een valide letter in:" << endl;
+        }//switch
 
-    } while ( keuzeGebruiker != 'C' || keuzeGebruiker != 'c' ||
-              keuzeGebruiker != 'R' || keuzeGebruiker != 'r' ||
-              keuzeGebruiker != 'P' || keuzeGebruiker != 'p' ||
-              keuzeGebruiker != 'S' || keuzeGebruiker != 's');
+    } while ( keuzeSubmenu != 'A' && keuzeSubmenu != 'a' &&
+              keuzeSubmenu != 'B' && keuzeSubmenu != 'b' &&
+              keuzeSubmenu != 'T' && keuzeSubmenu != 't' );
 
-    
+
+}//submenu
+
+
+void hoofdmenu () {
+
+    char keuzeHoofdmenu;
+
+    cout << "Welkom in het hoofdmenu. Maak een keuze uit: s(C)hoon, (R)andom, (P)arameters of (S)toppen" << endl;
+
+    while (keuzeHoofdmenu != 's' && keuzeHoofdmenu != 'S') {
+        cin  >> keuzeHoofdmenu;
+        switch (keuzeHoofdmenu) {
+
+            case 'C': case 'c':
+                cout << "U heeft C/c geantwoord" << endl;
+                break;
+            case 'R': case 'r':
+                cout << "U heeft R/r geantwoord" << endl;
+                break;
+            case 'P': case 'p':
+                submenu();
+                break;
+            default:
+                cout << "Vul een valide letter in:" << endl;
+        }//switch
+    }//while
 }//hoofdmenu
-
-
-
-
 
 
 
