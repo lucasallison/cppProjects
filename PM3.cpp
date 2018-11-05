d3ac11dba9360c87bebcaf4a19f8ef5bc85080b9
Lucas Allison <Lucasgallison@gmail.com>
2018-11-05 12:12:45 +0100

    added cin.get function

--- PM3.cpp
+++ PM3.cpp
@@ -1,5 +1,44 @@
 #include<iostream>
+#include<string>
+
 using namespace std;
+
+char gebruikerInvoer () {
+
+    char invoer;
+    cin.get(invoer);
+    while (invoer == '\n') {
+        cin.get(invoer);
+    }
+
+}
+
+int leesGetal(){
+
+    int max = 10000;
+    int getal = 0;
+    char invoer;
+    cout << "Vul iets in. " << endl;
+    cin.get(invoer);
+    while (invoer == '\n') {
+        cin.get(invoer);
+    }
+    while (invoer != '\n') {
+        if (isdigit(invoer)) {
+            getal = getal * 10 + invoer - '0';
+        }
+
+        if (getal > max) {
+            do {
+                getal = getal/10;
+            } while (getal > max);
+        }
+        cin.get(invoer);
+    }
+
+    return getal;
+}
+
 
 
 
@@ -10,7 +49,7 @@ void submenu () {
     cout << "Welkom in het submenu. Maak een keuze uit: A, B of T" << endl;
 
     do {
-        cin >> keuzeSubmenu;
+       keuzeSubmenu = gebruikerInvoer();
         switch (keuzeSubmenu) {
 
             case 'A': case 'a':
@@ -20,14 +59,13 @@ void submenu () {
                 cout << "U heeft B/b geantwoord" << endl;
                 break;
             case 'T':  case 't':
+                cout << "Welkom in het hoofdmenu. Maak een keuze uit: s(C)hoon, (R)andom, (P)arameters of (S)toppen" << endl;
                 break;
             default:
                 cout << "Vul een valide letter in:" << endl;
         }//switch
 
-    } while ( keuzeSubmenu != 'A' && keuzeSubmenu != 'a' &&
-              keuzeSubmenu != 'B' && keuzeSubmenu != 'b' &&
-              keuzeSubmenu != 'T' && keuzeSubmenu != 't' );
+    } while (keuzeSubmenu != 'T' && keuzeSubmenu != 't' );
 
 
 }//submenu
@@ -40,7 +78,7 @@ void hoofdmenu () {
     cout << "Welkom in het hoofdmenu. Maak een keuze uit: s(C)hoon, (R)andom, (P)arameters of (S)toppen" << endl;
 
     while (keuzeHoofdmenu != 's' && keuzeHoofdmenu != 'S') {
-        cin  >> keuzeHoofdmenu;
+      keuzeHoofdmenu = gebruikerInvoer();
         switch (keuzeHoofdmenu) {
 
             case 'C': case 'c':
@@ -60,9 +98,15 @@ void hoofdmenu () {
 
 
 
+
+
 int main () {
 
-    hoofdmenu();
+
+
+    cout << leesGetal();
+
+
 
     return 0;
 }
